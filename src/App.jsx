5be6063a17a03ea5a0a46018b71070db41fd2de9
isaiful508 <isaiful.islam508@gmail.com>
2024-03-18 21:39:55 +0600

    set up tailwind daisy and add navbr

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import NavBar from './components/DaisyNav/NavBar/NavBar'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+  
+
+  return (
+    <>
+     {/* <DaisyNav></DaisyNav> */}
+     <NavBar></NavBar>
+      <h1 className='text-7xl'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
